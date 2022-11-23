801b8c978e80e7062eeceb70b40066f325fe94c2
Ivan <sinenok.ivan@mail.ru>
2022-11-23 20:35:51 +0300

    Commit

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap';
 
 function App() {
-  return <div className="App"></div>;
+	return (
+		<div className="App">
+			<ButtonGroup aria-label="Basic example">
+				<Button variant="secondary">Left</Button>
+				<Button variant="secondary">Middle</Button>
+				<Button variant="secondary">Right</Button>
+			</ButtonGroup>
+		</div>
+	);
 }
 
 export default App;
