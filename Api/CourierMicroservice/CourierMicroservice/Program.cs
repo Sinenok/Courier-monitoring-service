d3815cbcbebfc307ed4dedb1b6721504c3a8870f
r.ali-isys <r.ali-isys@i-sys.ru>
2023-01-21 17:44:10 +0300

    Backend programCs refactoring

--- Api/CourierMicroservice/CourierMicroservice/Program.cs
+++ Api/CourierMicroservice/CourierMicroservice/Program.cs
@@ -3,29 +3,24 @@ using CourierMicroservice.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddScoped<IAuthorizationService, AuthorizationService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
-{
-    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
-}));
-
-builder.Services.AddDbContext<AppDbContext>(opt =>
-        opt.UseNpgsql(builder.Configuration.GetConnectionString("courierDB")));
-
+builder.Services.AddCors(p => p.AddPolicy("corsapp",
+                                          builder =>
+                                          {
+                                              builder.WithOrigins("*")
+                                                     .AllowAnyMethod()
+                                                     .AllowAnyHeader();
+                                          }));
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("courierDB")));
 var app = builder.Build();
-
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseCors("corsapp");
 app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
