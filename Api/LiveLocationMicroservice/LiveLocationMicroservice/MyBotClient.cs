0c740cf96623d4a809ceddab71acc83044e6dc06
Ramil <alimuahmed.ramil@gmail.com>
2023-05-28 14:11:14 +0300

    Add courier-monitoring-microservice.

---
+++ Api/LiveLocationMicroservice/LiveLocationMicroservice/MyBotClient.cs
@@ -0,0 +1,59 @@
+﻿using Telegram.Bot;
+
+namespace LiveLocationMicroservice;
+
+internal class MyBotClient
+{
+    public MyBotClient(string token) => BotClient = new TelegramBotClient(token);
+
+    private TelegramBotClient BotClient { get; }
+
+    [Obsolete("Obsolete")]
+    public void RunBot()
+    {
+        OnMessageSubscribe();
+        OnUpdateSubscribe();
+        BotClient.StartReceiving();
+    }
+
+    [Obsolete("Obsolete")]
+    private void OnMessageSubscribe()
+    {
+        BotClient.OnMessage += async (s, e) =>
+        {
+            try
+            {
+                if (e.Message.Location != null)
+                {
+                    await BotClient.SendTextMessageAsync(e.Message.Chat, $"Lat: {e.Message.Location?.Latitude}, Long: {e.Message.Location?.Longitude}");
+                }
+                else
+                {
+                    await BotClient.SendTextMessageAsync(e.Message.Chat, e.Message?.Text ?? "Unrecognized content");
+                }
+            }
+            catch
+            {
+            }
+        };
+    }
+
+    [Obsolete("Obsolete")]
+    private void OnUpdateSubscribe()
+    {
+        BotClient.OnUpdate += async (s, e) =>
+        {
+            try
+            {
+                if (e.Update.EditedMessage?.Location != null)
+                {
+                    await BotClient.SendTextMessageAsync(e.Update.EditedMessage.Chat,
+                                                         $"Lat: {e.Update.EditedMessage?.Location?.Latitude}, Long: {e.Update.EditedMessage?.Location?.Longitude}");
+                }
+            }
+            catch
+            {
+            }
+        };
+    }
+}
---
+++ Api/LiveLocationMicroservice/LiveLocationMicroservice/Program.cs
@@ -0,0 +1,14 @@
+﻿using LiveLocationMicroservice;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+IConfiguration config = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                                                  .AddJsonFile("appsettings.json", true)
+                                                  .Build();
+var telegramToken = config["TelegramToken"] ?? throw new JsonException("TelegramToken");
+var myBotClient = new MyBotClient(telegramToken);
+#pragma warning disable CS0618
+myBotClient.RunBot();
+#pragma warning restore CS0618
+Console.WriteLine("BotStarted");
+Console.ReadKey();
